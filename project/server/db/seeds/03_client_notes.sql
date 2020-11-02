978f6d6cb847f72a84cd5e128c5c1bdb2ffb51f2
mfarooq <maham.16491@gmail.com>
2020-11-01 19:19:32 -0500

    notes seeds added

--- project/server/db/seeds/03_client_notes.sql
+++ project/server/db/seeds/03_client_notes.sql
@@ -1,5 +1,13 @@
 INSERT INTO
   client_notes(notes, date,client_id)
 VALUES
-  ('Hey! Come back here! You big-a monkey!','01/02/2020',1),
-  ('Nice of the princess to invite us over a picnic, eh Luigi?','01/04/2020',2);
+  ('Lorem','08/17/2020',6),
+  ('Lorem ipsum dolor sit amet,','01/21/2020',9),
+  ('Lorem ipsum','09/01/2020',1),
+  ('Lorem','11/12/2020',4),
+  ('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur','07/12/2020',8),
+  ('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur','11/06/2020',3),
+  ('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed','09/17/2020',10),
+  ('Lorem ipsum dolor sit amet, consectetuer adipiscing','01/14/2021',7),
+  ('Lorem ipsum dolor sit amet, consectetuer adipiscing elit.','05/06/2020',2),
+  ('Lorem ipsum dolor','11/25/2020',5);
